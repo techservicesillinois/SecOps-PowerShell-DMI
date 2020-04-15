7185de5bb66236610451f26323f8121636ecc38c
MKletz <michael.kletz.27@gmail.com>
2020-04-15 13:19:49 -0500

    added missing dependency

--- .github/workflows/PublishToGallery.ps1
+++ .github/workflows/PublishToGallery.ps1
@@ -1,5 +1,6 @@
 try{
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    Install-Module -Name 'PSSQLite' -Force -SkipPublisherCheck
     Publish-Module -Path ".\UofIDMI" -Repository PSGallery -NuGetApiKey $ENV:NuGetApiKey -Force
 }
 catch{
