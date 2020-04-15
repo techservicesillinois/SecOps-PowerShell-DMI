1907690938e227d56076836e21e8e3e8151c2dfc
MKletz <michael.kletz.27@gmail.com>
2020-04-15 09:22:31 -0500

    removed unused variable

--- UofIDMI/UofIDMI.psm1
+++ UofIDMI/UofIDMI.psm1
@@ -3,8 +3,6 @@ param(
 )
 
 [String]$FunctionPath = Join-Path -Path $PSScriptRoot -ChildPath 'Functions'
-[String]$PublicFunctionPath = Join-Path -Path $FunctionPath -ChildPath 'Public'
-
 #All function files are executed while only public functions are exported to the shell.
 Get-ChildItem -Path $FunctionPath -Filter "*.ps1" -Recurse | ForEach-Object -Process {
     Write-Verbose -Message "Importing $($_.BaseName)" 
