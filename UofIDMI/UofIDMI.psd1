64bbd601fb24e02be812dfbb9c7bf2c12e3555f6
MKletz <michael.kletz.27@gmail.com>
2020-04-15 09:22:09 -0500

    Moved to explicit function exports vs dynamic

--- UofIDMI/UofIDMI.psd1
+++ UofIDMI/UofIDMI.psd1
@@ -69,7 +69,7 @@ RequiredModules = @('PSSQLite')
 # NestedModules = @()
 
 # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-FunctionsToExport = '*'
+FunctionsToExport = @('Get-DMIDepartment','Update-DMICache','New-DMISQLiteDB')
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
 CmdletsToExport = '*'
--- UofIDMI/UofIDMI.psm1
+++ UofIDMI/UofIDMI.psm1
@@ -7,13 +7,8 @@ param(
 
 #All function files are executed while only public functions are exported to the shell.
 Get-ChildItem -Path $FunctionPath -Filter "*.ps1" -Recurse | ForEach-Object -Process {
-    Write-Verbose -Message "Importing $($_.BaseName)"
-    
-    . $_.fullname
-
-    if($_.Directory -like "$($PublicFunctionPath)\*"){
-        Export-ModuleMember -Function $_.BaseName
-    }
+    Write-Verbose -Message "Importing $($_.BaseName)" 
+    . $_.FullName | Out-Null
 }
 
 [String]$SettingsPath = Join-Path -Path $PSScriptRoot -ChildPath 'Settings.json'
