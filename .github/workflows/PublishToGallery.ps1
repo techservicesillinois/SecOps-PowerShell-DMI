9cabf2b3c697e41ff164abb99a03e530a6a93a83
MKletz <michael.kletz.27@gmail.com>
2020-05-28 10:58:11 -0500

    missing force

--- .github/workflows/PublishToGallery.ps1
+++ .github/workflows/PublishToGallery.ps1
@@ -1,7 +1,7 @@
 try{
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
     Install-Module -Name 'PSDepend' -Force -SkipPublisherCheck
-    Invoke-PSDepend -Path "$($PSScriptRoot)\requirements.psd1"
+    Invoke-PSDepend -Path "$($PSScriptRoot)\requirements.psd1" -Force
     Publish-Module -Path ".\UofIDMI" -Repository PSGallery -NuGetApiKey $ENV:NuGetApiKey -Force
 }
 catch{
